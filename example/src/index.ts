8a52e8ad6016685b1b667350fc232fba150efb7e
ThejanNim <thejannims@gmail.com>
2024-01-30 16:02:14 +0530

    Create dummy package

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,11 +1,22 @@
-import express, { Express } from 'express';
+import express, { Express } from "express";
+const HelloPackage = require("express-swagger-annotations");
 
 const app: Express = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
+class HelloClass {
+  @HelloPackage()
+  hello() {
+    console.log("in hello method");
+  }
+}
+
+const tt = new HelloClass();
+tt.hello();
+
 app.listen(5000, () => {
-  console.log('Example app listening on port 3000!');
+  console.log("Example app listening on port 3000!");
 });
